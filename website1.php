8683e81c3f0150049799f8d1cbffdab86c422935
Prriya-Karn <101259927+Prriya-Karn@users.noreply.github.com>
2022-03-16 14:30:55 +0530

    Add files via upload

---
+++ website1.php
@@ -0,0 +1,633 @@
+<!doctype>
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="priya.css">
+	</head>
+	<body>
+		<!-- navigation bar -->
+	<div class="nav">
+<div class="logo">
+		<a href=""><img src="bg.png" style="width: 100px; height: 60px; position: absolute; left: 10%; top:8px;  border-radius:50%; "></a>
+		
+	</div>
+		<div class="n">
+		<a href="#">Bollywood</a>
+		<a href="#">Hollywood</a>
+		<a href="#">Hindi Web Series</a>
+		<a href="#">Cartoons</a>
+		<a href="#">Lyrical Songs</a>
+</div>
+	
+		<div class="search">
+			<form>
+				<input type="text" placeholder="Search">
+				<button class="sear">Search</button>
+			</form>
+		</div>
+	</div>
+	<br><br>
+      <!--latest section-->
+<div class="latest">
+	<h3 style="word-spacing: 2px; text-align:center ;">WWE Monday Kedarnath Full Movie 480p 24th January 2018 330MB</h3>
+</div>
+<br>
+
+<div class="icons">
+
+	<a href="#" class="facebook"><img  style="width: 100px; height: 50px; 	border-radius: 0px 50px 50px 0px; " src="rf.png"></a>
+	<a href="#" class="youtube"><img  style="width: 100px; height: 50px; 	border-radius: 0px 50px 50px 0px; " src="y3.png"></a>
+	<a href="#" class="twitter"><img  style="width: 100px; height: 50px; 	border-radius: 0px 50px 50px 0px; " src="t1.png"></a>
+	<a href="#" class="whatsapp"><img  style="width: 100px; height: 50px; 	border-radius: 0px 50px 50px 0px; " src="w1.jpg"></a>
+	
+</div>
+
+<!-- lyrics song -->
+<div class="wrapper">
+
+<div class="song">
+	<embed style= "width: 900px; height: 550px;"src="https://www.youtube.com/embed/dx4Teh-nv3A">
+		
+	</embed>
+	
+</div>
+<div class="p">
+<pre>         Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by   
+                       Abhishek Kapoor.Featuring Sushant Singh Rajput and newcomer Sara Ali Khan 
+                 in lead roles,it tells an inter-faith love story between a wealthy Hindu Brahmin girl it tells  
+                             an inter-faith love story between a wealthy Hindu Brahmin girl
+			       whose family owns a lodge  and shops near the historic Kedarnath Temple in the Uttarakhand 
+			            mountains and a Muslim boy who is a 'pithoo'(porter) working in the same vicinity.
+
+             Title : WWE Monday Kedarnath Full Movie 480p 24th January 2018 330MB
+
+                Rating: 10/10
+
+                Language: Hindi
+
+                Category: Movie
+
+                 Screenshots
+		</pre>
+	</div>
+
+
+</div>
+
+
+
+
+
+
+
+<!-- screenshot image -->
+<div class="main1">
+
+<div class="card1">
+
+<div class="image">
+
+<img style = "width:100% ; height:40%;"src="screenshot1.jpg"> 
+</div>
+</div>
+
+<div class="card1">
+
+<div class="image">
+  <img style = "width:100%; height:40%;" src="screenshot2.jpg">
+</div>
+</div>
+
+<div class="card1">
+
+<div class="image">
+ <img style = "width: 100%;height:40%;"  src="screenshot3.jpg"> 
+</div>
+</div>
+
+<div class="card1">
+
+<div class="image">
+  <img style = "width: 100%; height:40%;"  src="screenshot4.jpg"> 
+</div>
+</div>
+<br><br>
+ 
+<div class="download">
+ <a href="#">FULL SPEED DOWNLOAD</a>
+ </div>
+ <br><br><br><br>
+
+<hr>
+</div>
+<br>
+
+<!-- extra -->
+
+<div class="extra">
+
+<div class="download">
+	<a href="#">FAST SPEED DOWNLOAD</a>
+</div>
+<br><br>
+
+<div class="latest">
+	<h3>Related Movies</h3>
+</div>
+
+<br><br>
+
+
+<div class="main">
+
+<div class="card">
+
+<div class="image">
+  <a href="website1.php"> <img src="movie1.jpg"> </a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+<!--cards -->
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie2.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+<!--cards -->
+
+
+<div class="card">
+
+<div class="image">
+  <a href="website1.php"> <img src="movies3.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.Hindi</p>
+
+</div>
+</div>
+<!--cards -->
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+<!--cards -->
+
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+<!--cards -->
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+<!--cards -->
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+<!--cards -->
+
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+<!--cards -->
+
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+<br><br>
+<hr>
+</div>
+
+</div> 
+
+<!-- categories -->
+
+	<div class="latest">
+	<h3>Related Movies</h3>
+</div>
+<br><br>
+	<div class="wrapper1">
+
+<div class="slider" >
+	<!-- <img style= "width: 900px; height: 550px; background-image: url('Kedarnath.jpg'); background-repeat: no-repeat;" >
+ -->
+</div>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<hr>
+
+</div>
+<!-- <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<hr> -->
+
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br>
+
+<div class="latest">
+	<h3>Latest Movies</h3>
+</div>
+
+<br><br>
+
+
+<div class="main">
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+<!--cards -->
+
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+<!--cards -->
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+<!--cards -->
+
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+
+<!--cards -->
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+<!--cards -->
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+
+<!--cards -->
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+
+<!--cards -->
+
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+<!--cards -->
+
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+
+
+<div class="card">
+
+<div class="image">
+   <a href="website1.php"><img src="movie4.jpg"></a>
+</div>
+<div class="title">
+ <h1>
+Movie</h1>
+</div>
+<div class="des">
+ <p>Kedarnath is a 2018 Indian Hindi-language romantic drama disaster film written and directed by Abhishek Kapoor.Featuring Sushant Singh Rajput and Sara Ali Khan in lead roles.</p>
+
+</div>
+</div>
+<br><br>
+<hr>
+</div>
+
+</div> 
+
+</div>
+<br><br>
+<!-- footer -->
+
+<footer class="footer">
+	<div class="inner-footer">
+		
+
+
+		<div class="quick-links">
+		<ul>
+			<li class="quick-items"><a href="#">Home</a></li>
+			<li class="quick-items"><a href="#">Home</a></li>
+			<li class="quick-items"><a href="#">Home</a></li>
+			<li class="quick-items"><a href="#">Home</a></li>
+		</ul>
+</div>
+	</div>
+
+	<div class="outer-footer">
+		Copyright 2022 All RIghts Reserved
+	</div>
+
+</footer>
+
+
+
+	
+
+
+
+
+
+
+	</body>
+	</html>
